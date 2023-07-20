dbe9f2da30d49f5160c9a653252e18d390f4c245
sgratzl <sgratzl@users.noreply.github.com>
2023-07-20 01:58:59 +0000

    deploy: lineupjs/lineupjs@3d05b4db75e0472c60cb251d60514add162e2077

--- develop/src/model/MapColumn.d.ts
+++ develop/src/model/MapColumn.d.ts
@@ -1,7 +1,7 @@
 import type { IKeyValue, IMapColumn } from './IArrayColumn';
 import type { IDataRow, IValueColumnDesc } from './interfaces';
 import ValueColumn from './ValueColumn';
-export type IMapColumnDesc<T> = IValueColumnDesc<IKeyValue<T>[]>;
+export declare type IMapColumnDesc<T> = IValueColumnDesc<IKeyValue<T>[]>;
 export default class MapColumn<T> extends ValueColumn<IKeyValue<T>[]> implements IMapColumn<T> {
     constructor(id: string, desc: Readonly<IMapColumnDesc<T>>);
     getValue(row: IDataRow): IKeyValue<T>[];
